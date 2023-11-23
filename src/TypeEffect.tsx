7b875faf525898cdc5896a68e059fe4dfe1783d2
Idoko Agada Jerry <Idokoidoko4@gmail.com>
2023-11-23 19:24:15 +0100

    typeeffector

---
+++ src/TypeEffect.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import ssss from "./styleer/styleer.module.css";
+const wordsArr: string[] = ["Life is like an open book", "anybody can read it"];
+const TypeEffect: React.FC = () => {
+  const [wordIndex, setWordIndex] = useState<number>(0);
+  const [charIndex, setCharIndex] = useState<number>(0);
+  const [isDeleting, setIsDeleting] = useState<boolean>(false);
+  const dynamicTextRef = React.useRef<HTMLSpanElement>(null);
+  const [words,setWords] = useState<string[]>(wordsArr);
+
+
+  useEffect(() => {
+    const dynamicText = dynamicTextRef.current;
+
+    if (dynamicText) {
+      const currentWord = words[wordIndex];
+      const currentChar = currentWord.substring(0, charIndex);
+      dynamicText.innerHTML = currentChar;
+      dynamicText.classList.add("stop-blinking");
+
+      const typeTimeout = setTimeout(() => {
+        if (charIndex < currentWord.length && !isDeleting) {
+          setCharIndex(charIndex + 1);
+        } else if (charIndex > 0 && isDeleting) {
+          setCharIndex(charIndex - 1);
+        } else {
+          setIsDeleting(!isDeleting);
+          dynamicText.classList.remove("stop-blinking");
+
+          // If not deleting, wait for a pause before moving to the next word
+          if (!isDeleting) {
+            setTimeout(() => {
+              setWordIndex((prevIndex) => (prevIndex + 1) % words.length);
+            }, 4000); // Adjust this delay as needed
+          }
+        }
+      }, isDeleting ? 100 : 200);
+
+      return () => clearTimeout(typeTimeout);
+    }
+  }, [charIndex, isDeleting, wordIndex, words]);
+
+  return <h4 className={ssss.imTypingNow}><span ref={dynamicTextRef}></span></h4>;
+}
+
+export default TypeEffect;
